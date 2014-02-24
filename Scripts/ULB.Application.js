884ff2dec8bd245d9537f152896270d7c2dd2d68
Matt Bentley <matt.bentley@theguardian.com>
2014-02-24 15:32:12 +0000

    Angular project setup

---
+++ Scripts/ULB.Application.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="Libraries/angular.min.js" />
+/// <reference path="Libraries/angular-animate.min.js" />
+/// <reference path="Libraries/angular-touch.min.js" />
+/// <reference path="Libraries/angular-route.min.js" />
+
+var ulbApp = angular.module("ulb.app", ['ngRoute', 'ngAnimate', 'ngTouch']);
+
+/* Route
+===========================================================================================================================
+===========================================================================================================================*/
+
+ulbApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "Scripts/Templates/home-template.html",
+            controller: "HomeController"
+        })
+        .otherwise({
+            template: "<h1>http 404: not found</h1>"
+        });
+});
