a63c09a894a11aeb7b684b7cd578c79ae321496d
Matt Bentley <matt.bentley@theguardian.com>
2014-02-25 17:37:10 +0000

    Templates added View transitions Styling for home template Responsive css setup

--- Scripts/ULB.Application.js
+++ Scripts/ULB.Application.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="Libraries/angular.min.js" />
-/// <reference path="Libraries/angular-animate.min.js" />
-/// <reference path="Libraries/angular-touch.min.js" />
-/// <reference path="Libraries/angular-route.min.js" />
-
+﻿
 var ulbApp = angular.module("ulb.app", ['ngRoute', 'ngAnimate', 'ngTouch']);
 
 /* Route
@@ -14,6 +10,38 @@ ulbApp.config(function ($routeProvider) {
         .when("/", {
             templateUrl: "Scripts/Templates/home-template.html",
             controller: "HomeController"
+        })
+        .when("/question-one", {
+            templateUrl: "Scripts/Templates/q1-template.html",
+            controller: "QuestionOneController"
+        })
+        .when("/question-two", {
+            templateUrl: "Scripts/Templates/q2-template.html",
+            controller: "QuestionTwoController"
+        })
+        .when("/question-three", {
+            templateUrl: "Scripts/Templates/q3-template.html",
+            controller: "QuestionThreeController"
+        })
+        .when("/question-four", {
+            templateUrl: "Scripts/Templates/q4-template.html",
+            controller: "QuestionFourController"
+        })
+        .when("/question-five", {
+            templateUrl: "Scripts/Templates/q5-template.html",
+            controller: "QuestionFiveController"
+        })
+        .when("/question-six", {
+            templateUrl: "Scripts/Templates/q6-template.html",
+            controller: "QuestionSixController"
+        })
+        .when("/question-seven", {
+            templateUrl: "Scripts/Templates/q7-template.html",
+            controller: "QuestionSevenController"
+        })
+        .when("/results", {
+            templateUrl: "Scripts/Templates/results-template.html",
+            controller: "ResultsController"
         })
         .otherwise({
             template: "<h1>http 404: not found</h1>"
